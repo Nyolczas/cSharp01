ed5781a4151979736b979fd11cdecf811f273b2f
Nyolczas <nyolczas@gmail.com>
2018-11-07 21:36:00 +0100

    send mail (még nem jó)

--- nyolczasHu/nyolczasHu/Controllers/HomeController.cs
+++ nyolczasHu/nyolczasHu/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using nyolczasHu.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace nyolczasHu.Controllers
 {
@@ -22,7 +25,28 @@ namespace nyolczasHu.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(nyolczasHu.Models.gmail model)
+        {
+            MailMessage mm = new MailMessage("nyolczas@gmail.com", model.To);
+            mm.Subject = model.Subject;
+            mm.Body = model.Body;
+            mm.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 465;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("nyolczas@gmail.com", "gmailpassword");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mm);
+
+            ViewBag.Message = "Az üzenetet sikeresen elküldtük.";
 
             return View();
         }
