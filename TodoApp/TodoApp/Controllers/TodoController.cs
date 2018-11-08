98e4b6e0444c4df516b6e4c7bece601501f8456f
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-11-08 14:46:00 +0100

    rögzítés Ideiglenes adatbázissal

--- TodoApp/TodoApp/Controllers/TodoController.cs
+++ TodoApp/TodoApp/Controllers/TodoController.cs
@@ -9,18 +9,23 @@ namespace TodoApp.Controllers
 {
     public class TodoController : Controller
     {
+        
+
         public ActionResult Index()
         {
-            // feladat lista 
-            var Lista = new List<CelFeladat>();
+            return View(MyDb.Lista);
+        }
 
-            Lista.Add(new CelFeladat() { name = "anyagi biztonság", done = true });
-            Lista.Add(new CelFeladat() { name = "anyagi függetlenség", done = false });
-            Lista.Add(new CelFeladat() { name = "házépítés", done = false });
-            Lista.Add(new CelFeladat() { name = "jogosítvány megszerzése", done = false });
-            Lista.Add(new CelFeladat() { name = "vállalkozói karrier építése", done = false });
-
-            return View(Lista);
+        public ActionResult Create(string ujCel)
+        {
+            // ha visszatér valamilyen értékkel,
+            if(!string.IsNullOrEmpty(ujCel))
+            {
+                // Akkor hozzáadjuk a listánkhoz az új tartalmat.
+                MyDb.Lista.Add(new CelFeladat() { name = ujCel, done = true });
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
---
+++ TodoApp/TodoApp/Models/MyDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoApp.Models
+{
+    public class MyDb
+    {
+        /// <summary>
+        /// Ideiglenes barkács megoldás a végleges adatbázis elkészültéig
+        /// </summary>
+        public static List<CelFeladat> Lista = new List<CelFeladat>
+            {
+                new CelFeladat() { name = "anyagi biztonság", done = true },
+                new CelFeladat() { name = "anyagi függetlenség", done = false },
+                new CelFeladat() { name = "házépítés", done = false },
+                new CelFeladat() { name = "jogosítvány megszerzése", done = false },
+                new CelFeladat() { name = "vállalkozói karrier építése", done = false }
+            };
+    }
+}
