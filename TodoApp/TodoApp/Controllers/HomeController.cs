4bd7b4db04b3706644a3ca9aa7e7f5ab5579b496
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-11-08 13:31:12 +0100

    Todo Controller áthelyezések

--- TodoApp/TodoApp/Controllers/HomeController.cs
+++ TodoApp/TodoApp/Controllers/HomeController.cs
@@ -27,19 +27,5 @@ namespace TodoApp.Controllers
 
             return View();
         }
-
-        public ActionResult FeladatLista()
-        {
-            // feladat lista 
-            var Lista = new List<CelFeladat>();
-
-            Lista.Add(new CelFeladat() { name = "anyagi biztonság", done = true });
-            Lista.Add(new CelFeladat() { name = "anyagi függetlenség", done = false });
-            Lista.Add(new CelFeladat() { name = "házépítés", done = false });
-            Lista.Add(new CelFeladat() { name = "jogosítvány megszerzése", done = false });
-            Lista.Add(new CelFeladat() { name = "vállalkozói karrier építése", done = false });
-
-            return View(Lista);
-        }
     }
 }
---
+++ TodoApp/TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        public ActionResult Index()
+        {
+            // feladat lista 
+            var Lista = new List<CelFeladat>();
+
+            Lista.Add(new CelFeladat() { name = "anyagi biztonság", done = true });
+            Lista.Add(new CelFeladat() { name = "anyagi függetlenség", done = false });
+            Lista.Add(new CelFeladat() { name = "házépítés", done = false });
+            Lista.Add(new CelFeladat() { name = "jogosítvány megszerzése", done = false });
+            Lista.Add(new CelFeladat() { name = "vállalkozói karrier építése", done = false });
+
+            return View(Lista);
+        }
+    }
+}
