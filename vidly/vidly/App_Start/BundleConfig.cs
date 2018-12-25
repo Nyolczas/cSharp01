8f07f2ce4bfac7e22a7708166bfe63a0cd0a6ca5
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-12-25 15:24:28 +0100

    vidly custom bootstrap theme

--- vidly/vidly/App_Start/BundleConfig.cs
+++ vidly/vidly/App_Start/BundleConfig.cs
@@ -22,8 +22,10 @@ namespace vidly
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-superhero.css",
                       "~/Content/site.css"));
         }
     }
