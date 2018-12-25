46e4e288f6d1b60a35727a63d3dceb98f4cd3e6e
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-12-25 23:22:00 +0100

    vidly

--- vidly/vidly/Controllers/MoviesController.cs
+++ vidly/vidly/Controllers/MoviesController.cs
@@ -13,9 +13,11 @@ namespace vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
+
             return View(movie);
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "." + month);
