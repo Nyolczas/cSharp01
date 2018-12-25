8fc2c98bc7669cdf77f369d3ac1bf145afb539d3
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-12-25 17:07:03 +0100

    movies controller

--- vidly/vidly/Controllers/MoviesController.cs
+++ vidly/vidly/Controllers/MoviesController.cs
@@ -15,5 +15,25 @@ namespace vidly.Controllers
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "." + month);
+        }
+
+        // ez automatikusan létrehozza az oldalt
+        public ActionResult Edit(int id) 
+        {
+            return Content("id=" + id);
+        }
+
+        // movies 
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue) pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
